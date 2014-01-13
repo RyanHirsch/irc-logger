5399049a177d65d95e0a15fb22336e2c959e91a5
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2014-01-13 07:47:33 -0600

    Working join command

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,12 @@
 console.log('Loading join.js');
 module.exports = function(options) {
-    options.bot.say(options.to, options.message);
-    console.log('JOIN: ' + options);
+    var bot = options.bot;
+    var message = options.message;
+
+    if(message.substr(0,1) !== '#') {
+        bot.say(options.to, message + " doesn't seem like a valid channel to me.");
+    }
+    else {
+        bot.join(message);
+    }
 };
