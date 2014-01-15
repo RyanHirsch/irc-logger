590346194d3d026864976024e2035759f52acf82
Ryan Hirsch <Ryan.Hirsch@gmail.com>
2014-01-15 17:06:10 -0600

    Prep for testing and allow for 'mention' commands along with invoke_string

--- commands.js
+++ commands.js
@@ -1,35 +1,54 @@
 var _ = require('lodash'),
     fs = require('fs');
 var invoke_string = '!log';
-var commands = {
-//    join    : require('./commands/join'),
-//    leave   : require('./commands/leave'),
-};
+var commands = { };
 
 // rehash and reload our commands
 var rehash = function(directory) {
-    var path = __dirname + '/' + directory,
-        hashMatch = new RegExp('^' + path + '/', 'gi');
+    var path = __dirname + '/' + directory;
+    clearCache(path);
+    loadModulesFrom(directory);
+};
+
+var clearCache = function(path) {
+    var hashMatch = new RegExp('^' + path + '/', 'gi');
 
     _.forEach(require.cache, function(item) {
         if(item.id.match(hashMatch)) {
+            console.log(item.id);
             delete require.cache[item.id];
         }
     });
+};
+
+var loadModulesFrom = function(directory) {
     fs.readdir(directory, function(err, files) {
         if(err) console.log('ERROR: ' + err);
+
+        commands = {};
         _.forEach(files, function(file) {
             // join, leave, quit
             var fileName = file.substring(0, file.length - 3)
             commands[fileName] = require('./' + directory + '/' + fileName);
         });
     });
+};
 
+var isToMe = function(bot, firstToken) {
+    var m = new RegExp('^' + bot + '[:,\-]?', 'gi');
+
+    if(firstToken.match(m)) {
+        console.log('true');
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 var processMessage = function(bot, from, to, message) {
     var tokens = message.split(' ');
-    if(!_.isArray(tokens) || tokens[0] !== invoke_string) { return; }
+    if(!_.isArray(tokens) || !(tokens[0] === invoke_string || isToMe(bot.nick, tokens[0]))) { return; }
     if(commands.hasOwnProperty(tokens[1])) {
         var joinedTokens = tokens.slice(2).join(' ');
 
@@ -41,7 +60,20 @@ var processMessage = function(bot, from, to, message) {
             message: joinedTokens
         });
     }
+    else {
+        bot.say(to, "sorry I don't support " + tokens[1]);
+    }
 }
+
+if (process.env.NODE_ENV === "test") {
+   exports._rehash = rehash;
+   exports._clearCache = clearCache;
+   exports._loadModulesFrom = loadModulesFrom;
+   exports._isToMe = isToMe;
+}
+
+loadModulesFrom('./commands');
 exports.processMessage = processMessage;
 exports.rehash = function() { rehash('commands'); };
+
 // !log leave <channel>
